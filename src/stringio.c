ea2db925fed853675a7b77e168d7f091f1f0dee0
David Collett <david.collett@gmail.com>
2006-08-06 01:14:59 -0700

    add licence to all files

--- src/stringio.c
+++ src/stringio.c
@@ -1,8 +1,28 @@
+/*
+  This code is from the pyflag project:
+  http://pyflag.sourceforge.net/
+  
+  Copyright (C) Michael Cohen (scudette@users.sourceforge.net)
+  
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
 #include <string.h>
 #include <ctype.h>
 #include "stringio.h"
 #include "talloc.h"
-//#include "misc.h"
 
 StringIO StringIO_constructor(StringIO self) {
   //Create a valid buffer to hold the data:
