cd384fa8f143952472676f813f45e1ef2aaab0e3
Kristofer Henriksson <kthenriksson@gmail.com>
2012-03-13 15:07:55 -0400

    Revert "Add Mac note for autogen.sh"
    This reverts commit f220ae1ef5c13b82f6b916ce06bfe43cf173fe69. It turns out
    we don't need this on Xcode 4.3 and latest autoconf.

--- autogen.sh
+++ autogen.sh
@@ -3,7 +3,3 @@
 # We can just let autoreconf do what is needed
 echo 'Running autoreconf --install'
 autoreconf --install
-
-# On a Mac with Homebrew, the following command is needed:
-# autoreconf --install -I /usr/local/share/aclocal
-# This will require autoconf from homebrew-alt
