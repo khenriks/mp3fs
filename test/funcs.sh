55ea5180342d7f693f8e8bc6f6fb91bc6150ca86
Kristofer Henriksson <kthenriksson@gmail.com>
2012-03-12 22:34:22 -0400

    Fix tests for POSIX shell compliance

--- test/funcs.sh
+++ test/funcs.sh
@@ -1,12 +1,13 @@
 PATH=$PWD/../src:$PATH
+export LC_ALL=C
 
-function setup {
+setup () {
 	mkdir /tmp/mp3
 	mp3fs -d "$PWD/flac" /tmp/mp3 2>$0-debug.log &
 	sleep 0.1
 }
 
-function finish {
+finish () {
 	hash fusermount 2>&- && fusermount -u /tmp/mp3 || umount /tmp/mp3
 	rmdir /tmp/mp3
 }
