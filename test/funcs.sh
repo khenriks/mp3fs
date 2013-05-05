aa12cb9a014d20c731038fa45825f1888629cedc
Kristofer Henriksson <kthenriksson@gmail.com>
2013-05-05 14:19:37 -0400

    Use mktemp to generate temporary directory name

--- test/funcs.sh
+++ test/funcs.sh
@@ -1,12 +1,12 @@
 PATH=$PWD/../src:$PATH
 
 function setup {
-	mkdir /tmp/mp3
-	mp3fs -d "$PWD/flac" /tmp/mp3 2>$0-debug.log &
+    DIRNAME="$(mktemp -d)"
+    mp3fs -d "$PWD/flac" "$DIRNAME" 2>$0-debug.log &
 	sleep 0.1
 }
 
 function finish {
-	hash fusermount 2>&- && fusermount -u /tmp/mp3 || umount /tmp/mp3
-	rmdir /tmp/mp3
+    hash fusermount 2>&- && fusermount -u "$DIRNAME" || umount "$DIRNAME"
+    rmdir "$DIRNAME"
 }
