5fe64e252ded2e86618d6533c5fed28495dcb627
Kristofer Henriksson <kthenriksson@gmail.com>
2013-05-05 14:30:09 -0400

    Check better for availability of mount

--- test/funcs.sh
+++ test/funcs.sh
@@ -3,7 +3,9 @@ PATH=$PWD/../src:$PATH
 function setup {
     DIRNAME="$(mktemp -d)"
     mp3fs -d "$PWD/flac" "$DIRNAME" 2>$0-debug.log &
-	sleep 0.1
+    while ! mount | grep -q "$DIRNAME" ; do
+        sleep 0.1
+    done
 }
 
 function finish {
