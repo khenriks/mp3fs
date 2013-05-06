2899093d85c3b9bcf00e7bdd2b4e6df9cbff6eec
Kristofer Henriksson <kthenriksson@gmail.com>
2013-05-05 23:22:29 -0400

    Signal to test system when mp3fs exits with error
    Also improve logic in funcs.sh to simplify the body of test scripts.

--- test/funcs.sh
+++ test/funcs.sh
@@ -1,17 +1,24 @@
 PATH=$PWD/../src:$PATH
 
-function setup {
-    set -e
-    trap finish ERR
-
-    DIRNAME="$(mktemp -d)"
-    mp3fs -d "$PWD/flac" "$DIRNAME" 2>$0-debug.log &
-    while ! mount | grep -q "$DIRNAME" ; do
-        sleep 0.1
-    done
-}
-
-function finish {
-    hash fusermount 2>&- && fusermount -u "$DIRNAME" || umount "$DIRNAME" || :
+function cleanup {
+    EXIT=$?
+    # Errors are no longer fatal
+    set +e
+    hash fusermount 2>&- && fusermount -u "$DIRNAME" || umount "$DIRNAME"
     rmdir "$DIRNAME"
+    exit $EXIT
 }
+
+function mp3fserr {
+    exit 99
+}
+
+set -e
+trap cleanup EXIT
+trap mp3fserr USR1
+
+DIRNAME="$(mktemp -d)"
+( mp3fs -d "$PWD/flac" "$DIRNAME" || kill -USR1 $$ ) &
+while ! mount | grep -q "$DIRNAME" ; do
+    sleep 0.1
+done
