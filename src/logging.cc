c1b64d8c5b9a591a0daf2d4134458e623c1b643f
K Henriksson <kthenriksson@gmail.com>
2017-04-21 20:02:47 -0700

    Add logging class for mp3fs
    This will be used to replace the current pure-syslog based approach. It
    is part of the work for #10.

---
+++ src/logging.cc
@@ -0,0 +1,82 @@
+/*
+ * Logging class source for mp3fs
+ *
+ * Copyright (C) 2017 K. Henriksson
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#include "logging.h"
+
+#include <ctime>
+#include <iostream>
+
+#include <syslog.h>
+
+namespace {
+    Logging* logging;
+}
+
+Logging::Logging(std::string logfile, level max_level, bool to_stderr,
+                 bool to_syslog) :
+    max_level_(max_level), to_stderr_(to_stderr), to_syslog_(to_syslog) {
+    if (!logfile.empty()) {
+        logfile_.open(logfile);
+    }
+    if (to_syslog_) {
+        openlog("mp3fs", 0, LOG_USER);
+    }
+}
+
+Logging::Logger::~Logger() {
+    if (!logging_ || loglevel_ > logging_->max_level_) return;
+
+    // Construct string containing time
+    std::time_t now = std::time(nullptr);
+    std::string time_string(30, '\0');
+    time_string.resize(std::strftime(&time_string[0], time_string.size(),
+                                     "%F %T", std::localtime(&now)));
+
+    std::string msg = "[" + time_string + "] " +
+        level_name_map_.at(loglevel_) + ": " + str();
+
+    if (logging_->to_syslog_) {
+        syslog(syslog_level_map_.at(loglevel_), "%s", msg.c_str());
+    }
+    if (logging_->logfile_.is_open()) logging_->logfile_ << msg << std::endl;
+    if (logging_->to_stderr_) std::clog << msg << std::endl;
+}
+
+const std::map<Logging::level,int> Logging::Logger::syslog_level_map_ = {
+    {ERROR, LOG_ERR},
+    {INFO, LOG_INFO},
+    {DEBUG, LOG_DEBUG},
+};
+
+const std::map<Logging::level,std::string> Logging::Logger::level_name_map_ = {
+    {ERROR, "ERROR"},
+    {INFO, "INFO"},
+    {DEBUG, "DEBUG"},
+};
+
+Logging::Logger Log(Logging::level lev) {
+    return {lev, logging};
+}
+
+bool InitLogging(std::string logfile, Logging::level max_level, bool to_stderr,
+                 bool to_syslog) {
+    logging = new Logging(logfile, max_level, to_stderr, to_syslog);
+    return !logging->GetFail();
+}
---
+++ src/logging.h
@@ -0,0 +1,80 @@
+/*
+ * Logging class header for mp3fs
+ *
+ * Copyright (C) 2017 K. Henriksson
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#ifndef LOGGING_H
+#define LOGGING_H
+
+#include <array>
+#include <fstream>
+#include <map>
+#include <ostream>
+#include <sstream>
+
+class Logging {
+public:
+    enum class level { ERROR = 1, INFO = 2, DEBUG = 3 };
+
+    /*
+     * Arguments:
+     *   logfile: The name of a file to write logging output to. If empty, no
+     *     output will be written.
+     *   max_level: The maximum level of log output to write.
+     *   to_stderr: Whether to write log output to stderr.
+     *   to_syslog: Whether to write log output to syslog.
+     */
+    explicit Logging(std::string logfile, level max_level, bool to_stderr,
+                     bool to_syslog);
+
+    bool GetFail() const { return logfile_.fail(); }
+
+private:
+    class Logger : public std::ostringstream {
+    public:
+        Logger(level loglevel, Logging* logging) :
+            loglevel_(loglevel), logging_(logging) {}
+        Logger() : loglevel_(level::DEBUG) {}
+        ~Logger();
+
+    private:
+        const level loglevel_;
+
+        Logging* logging_;
+
+        static const std::map<level,int> syslog_level_map_;
+        static const std::map<level,std::string> level_name_map_;
+    };
+
+    friend Logger Log(level lev);
+    friend Logger;
+
+    std::ofstream logfile_;
+    const level max_level_;
+    const bool to_stderr_;
+    const bool to_syslog_;
+};
+
+bool InitLogging(std::string logfile, Logging::level max_level, bool to_stderr,
+                 bool to_syslog);
+
+constexpr auto ERROR = Logging::level::ERROR;
+constexpr auto INFO = Logging::level::INFO;
+constexpr auto DEBUG = Logging::level::DEBUG;
+
+#endif
